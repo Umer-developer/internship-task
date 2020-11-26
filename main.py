70a9ace2be85ebd408d59236cfd18c694a56b601
Umer <umerbasharat0@gmail.com>
2020-11-26 09:59:28 +0500

    date and time

---
+++ main.py
@@ -0,0 +1,11 @@
+import datetime
+
+print ('The Date Today is  :', datetime.datetime.today())
+
+date_today = datetime.date.today()
+print (date_today)
+print ('This Year   :', date_today.year)
+print ('This Month    :', date_today.month)
+
+print ('This Week Day    :', date_today.day)
+
